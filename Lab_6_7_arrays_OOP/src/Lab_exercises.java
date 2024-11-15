01a52b4b65de9d0ae40d16cd606bf6617497ccda
Huma Gonen <huma.gonen10@yahoo.com>
2024-11-15 18:47:03 +0000

    Update Lab_exercises.java

--- Lab_6_7_arrays_OOP/src/Lab_exercises.java
+++ Lab_6_7_arrays_OOP/src/Lab_exercises.java
@@ -2,41 +2,81 @@ import java.util.ArrayList;
 
 public class Lab_exercises {
 	public static void main(String args[]){
-		
+
 		// this is not possible:
-//		double x = 10;
-//		int y = x;
-//		System.out.println(y);
-		
+		//		double x = 10;
+		//		int y = x;
+		//		System.out.println(y);
+
 		// correct way:
 		double x = 10.5;
 		int y = (int) x;
 		System.out.println(x);
 		System.out.println(y);
-		
+
 		char letter = 'A';
 		int ascii = (int) letter;
 
 		System.out.println(ascii);
+		System.out.println();
+
+
+		// Q.6.7.2
+
+		ArrayList<Data> ArrayA = new ArrayList<Data>();
+
+		ArrayA.add(new Data("Fred", 21));
+		ArrayA.add(new Data("Jo", 43));
+		ArrayA.add(new Data("Zoe", 37));
+//		ArrayA.add(2, new Data("Harry", 71)); // inserting to specific index
+
+		ArrayList<Data> ArrayB = new ArrayList<Data>();
+		
+		System.out.print("ArrayA:\n");
+		PrintDataArray(ArrayA);
+		System.out.println();
+
+		ArrayB = ArrayA;  // copied ArrayA to ArrayB
+		System.out.print("ArrayB:\n");
+		PrintDataArray(ArrayB);
+		System.out.println();
+
+		ArrayA.remove(1);  // Removed the the specific indexed value: index 1 which is Jo
+		System.out.print("ArrayB:\n");
+		PrintDataArray(ArrayB);
+		System.out.println();
 		
 		
-	// Q6.7.2.1
+		ArrayList<Data> ArrayC = new ArrayList<Data>();
+		ArrayList<Data> ArrayD = new ArrayList<Data>();
 		
-		ArrayList<Data> people = new ArrayList<Data>();
+		System.out.print("ArrayA:\n");
+		PrintDataArray(ArrayA);
+		System.out.println();
 		
-		people.add(new Data("Fred", 21));
-		people.add(new Data("Jo", 43));
-		people.add(new Data("Zoe", 37));
-		people.add(2, new Data("Harry", 71)); // inserting to specific index
+		ArrayC = ArrayA;  // copied ArrayA to ArrayC
+		System.out.print("ArrayC:\n");
+		PrintDataArray(ArrayC);
+		System.out.println();
 		
-		PrintDataArray(people);
+		ArrayD = (ArrayList<Data>)ArrayC.clone(); // if we use clone instead of assigning each 
+// arrays to each other, then differences made in one array will not effect the other
+		ArrayC.remove(1);
+		System.out.print("ArrayC:\n");
+		PrintDataArray(ArrayC);
+		System.out.println();
 		
-	// Q6.7.2.2
+		System.out.print("ArrayD:\n");
+		PrintDataArray(ArrayD);
+		System.out.println(); 
 		
 		
+		// Q.6.7.3 Swiftbot
+		
+
 	}
 
-	
+
 	private static void PrintDataArray(ArrayList<Data> array) 
 	{
 		for(Data e: array) 
